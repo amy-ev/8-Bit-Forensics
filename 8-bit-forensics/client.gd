e65df3a4498a53d1f2b6d549772b54c5312e6a67
amy-ev <amyevemy@hotmail.co.uk>
2025-04-23 19:02:03 +0100

    file name increments on img send and clearer file structure

--- 8-bit-forensics/client.gd
+++ 8-bit-forensics/client.gd
@@ -1,5 +1,4 @@
 extends Node
-
 const HOST = "127.0.0.1"
 const PORT = 8000
 var client = StreamPeerTCP.new()
@@ -14,81 +13,48 @@ func _process(delta: float) -> void:
 	if status != client.STATUS_CONNECTED:
 		client.poll()
 		print("not connected yet, status: ", status)
-	#print(status)
-	var file = open_img("example.jpg")
-	var data = send_data(client,file)
-	print(client.get_available_bytes())
-	if client.get_available_bytes():
-		var response = client.get_data(client.get_available_bytes())
-		print("recieved the fucking response: ", response)
 		
-		
-	#var response = recv_data(client)
-	#print(response.get_string_from_utf8())
+	client.poll()
 	
+	print("status: ", status)
+	# --- send data ---
+	#var msg = "hello"
+	#var msg_bytes = msg.to_utf8_buffer()
+	
+	var msg_bytes = open_img("res://example.jpg")
+	send_data(msg_bytes)
+	recv_data()
+	# --- receive data ---
+		
 func open_img(img_path):
 	#print(OS.get_executable_path().get_base_dir().path_join("example.jpg"))
 	#print(ProjectSettings.globalize_path("res://example.jpg"))
 	if FileAccess.file_exists(img_path):
 		var file = FileAccess.open(img_path, FileAccess.READ)
-		return file
+		var img_data = file.get_buffer(file.get_length())
+		return img_data
 	else:
 		print("failed to open file")
 		return
-	
-func send_data(socket, file):
-	var packet_size = file.get_length()
-	var data = file.get_buffer(packet_size)
-	
-	var buffer = PackedByteArray()
-	buffer.resize(4)
-	buffer.encode_u32(0, packet_size)
-	
-	var packet = buffer + data
-	socket.poll()
-	socket.put_data(packet)
-	
-	print("img sent")
+		
+func send_data(msg_bytes):
+	print(msg_bytes.size())
 
-func recv_msg(socket):
-	pass
-	#var msg_len = recv_data(socket,4)
-	#if msg_len.size() != 4:
-		#return null
-	#msg_len = msg_len.decode_u32(0)
-	#return recv_data(socket, msg_len)
-	#
-	#num_bytes
-func recv_data(socket):
-	var header = socket.get_data(4)
-	var msg_len = header.decode_u32(0)
+	var data_buffer = StreamPeerBuffer.new()
+	data_buffer.big_endian = true
+	data_buffer.put_u32(msg_bytes.size()) # 32-bit unsigned = 4 bytes
 	
-	var data = PackedByteArray()
-	while data.size() < msg_len:
-		var packet = socket.get_data(4096)
-		print(packet)
-		if not data:
-			break
-		data.append(packet)
-	return data
-	#print(num_bytes)
-	#while data.size() < num_bytes:
-		#var remaining = num_bytes - data.size()
-		#print(remaining)
-		#var data_status = socket.get_data(remaining)
-		#if data_status[0] != OK:
-			#break
-		#var packet = data_status[1]
-		#if packet.size() == 0:
-			#break
-		#data.append_array(packet)
-		#print("the data:", data)
-	#return data
+	var msg_len = data_buffer.data_array
+	client.put_data(msg_len) # send the size of the packet a 4 bytes
+	client.put_data(msg_bytes) # send the packet
+
+func recv_data():
+	if client.get_available_bytes() != 0:
+
+		var response = client.get_utf8_string(client.get_available_bytes())
+		print(response)
+		client.disconnect_from_host()
+		queue_free()
+	else:
+		client.poll()
 		
-		
-		
-	#var output = []
-	#OS.execute("CMD.exe",["/C", "python C:/Users/Amy/Desktop/8-Bit-Forensics/godot/8-bit-forensics/venv/Scripts/activate.bat"], [])
-	##var results = OS.execute("CMD.exe",["/C", "python C:/Users/Amy/Desktop/8-Bit-Forensics/client.py"], output, true,false)
-	#print(output)
-	
--- 8-bit-forensics/python_files/server.py
+++ 8-bit-forensics/python_files/server.py
@@ -1,70 +1,53 @@
 import socket
 import struct
-import binascii
-
-host = "127.0.0.1"
-port = 8000
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 def _ready():
 
+    host = '127.0.0.1'
+    port = 8000
+
+    file_name = ""
+    with open("index.txt","r") as i:
+        file_no = int(i.read())
+    i.close()
+    
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
-    server.listen(1) # maybe 0
+    server.listen(0) # maybe 0
 
-    print(f"[+] listening on port {host}:{port}")
+    print("server listening")
 
     while True:
         client_socket, client_address = server.accept()
-        print(f"[+] accepted connection from: {client_address}")
-        msg = recv_msg(client_socket)
 
-        # will ONLY reach this point if the client disconnects
+        print(f"connected to: {client_address}")
+        # --- receive data ---
+        msg_len = struct.unpack("!I", client_socket.recv(4))[0]
+        data = bytearray()
 
-        # print("recieved")
-        # response = "kill me".encode()
-        # print("sending response")
-        # send_data(client_socket, response)
-        # print("sent")
-        #print(binascii.hexlify(msg).decode())
+        while len(data) < msg_len:
+            packet = client_socket.recv(msg_len - len(data))
+            if not packet:
+                break
+            data.extend(packet)
 
-        with open("hex.txt","w") as hex_text:
-            hex_text.write(binascii.hexlify(msg).decode())
-        hex_text.close()
+        print(len(data))
+        
+        # --- send response ---
+        client_socket.sendall("I might actually cry".encode())
 
-        with open("sent.jpg", "wb") as img:
-            img.write(msg)
-        img.close()
+        file_name = "file_"+ str(file_no).strip() + ".jpg"
+        with open(file_name,"wb") as img_file:
+            img_file.write(data)
+        img_file.close()
 
-        response = "accepted".encode("utf-8")
-        send_data(client_socket, response)
-        print("got here")
-    client_socket.close()
+        file_no = file_no + 1
+        with open("index.txt","w") as i:
+            i.write(str(file_no))
+        i.close()
 
-
-def recv_msg(client_socket):
-    msg_len = recv_data(client_socket, 4)
-    if not msg_len or len(msg_len) < 4:
-        print("failed to read msg length")
-        return None
-    msg_len = struct.unpack("!I", msg_len)[0] #big-endian unsigned int
-    print("msg length: ", msg_len)
-
-    return recv_data(client_socket, msg_len)
-
-def recv_data(client_socket, num_bytes):
-    data = bytearray()
-    while len(data) < num_bytes:
-        packet = client_socket.recv(num_bytes - len(data))
-        #print(packet)
-        if not packet:
-            break
-        data.extend(packet) # byte array used extend not append
-    
-    return data
-
-def send_data(socket, data):
-    data = struct.pack("!I", len(data)) + data
-    socket.sendall(data)
 
 if __name__ == "__main__":
     _ready()
