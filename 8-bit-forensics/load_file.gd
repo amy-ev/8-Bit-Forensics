4f31e45e9ce39a2d0d5984d4d60058d843ee348f
amy-ev <amyevemy@hotmail.co.uk>
2025-04-25 14:24:40 +0100

    signals implemented

--- 8-bit-forensics/load_file.gd
+++ 8-bit-forensics/load_file.gd
@@ -2,11 +2,16 @@ extends Control
 
 
 func _on_load_button_pressed() -> void:
-	pass
+	$FileDialog.popup()
 	
 
 
 func _on_file_dialog_file_selected(path: String) -> void:
 	var image = Image.new()
 	image.load(path)
+	
+	var image_text = ImageTexture.new()
+	image_text.set_image(image)
+	
+	$files/TextureRect.texture = image_text
 	
