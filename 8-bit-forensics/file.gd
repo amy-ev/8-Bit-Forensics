3477455ce7db67bd3b113abb33aca95153fca4f6
amy-ev <amyevemy@hotmail.co.uk>
2025-04-25 20:42:00 +0100

    files dynamically loaded with icon and file names based on existing folder

--- 8-bit-forensics/file.gd
+++ 8-bit-forensics/file.gd
@@ -2,11 +2,17 @@ extends Control
 class_name File
 
 @export var file_name: String 
+@export var file_icon: ImageTexture
 
 func _ready() -> void:
-	file_name = $icon.texture.resource_path.get_file()
-	$file_name.text = file_name
+	pass
+	#file_name = $icon.texture.resource_path.get_file()
 
+func _process(delta: float) -> void:
+	$file_name.text = file_name
+	$icon.texture = file_icon
+	
 func _on_select_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton && event.is_pressed():
-		print(file_name)
+		get_parent().get_parent().get_node("load_button").text = file_name
+		#$file_name.text = file_name
--- 8-bit-forensics/load_file.gd
+++ 8-bit-forensics/load_file.gd
@@ -1,17 +1,44 @@
 extends Control
 
+@onready var files = preload("res://file.tscn")
+@export var _file_name: String
+@export var _file_icon: ImageTexture
 
+@export var selected_file: String
+var _file: File
+
+
+func _ready() -> void:
+	add_files(file_count("res://jpg_folder/"))
+	
+	
 func _on_load_button_pressed() -> void:
-	$FileDialog.popup()
+	print($load_button.text)
+	selected_file = $load_button.text
 	
+func add_files(file_no:int):
+	for i in file_no:
+		_file = files.instantiate()
+		$GridContainer.add_child(_file)
+		_file.name = "file"
+		_file.file_name = _file.name
+		_file_icon = ImageTexture.create_from_image(Image.load_from_file("res://jpg_folder/photo"+str(i)+".jpg"))
+		_file.file_icon = _file_icon
 
 
-func _on_file_dialog_file_selected(path: String) -> void:
-	var image = Image.new()
-	image.load(path)
+func file_count(file_path:String) -> int:
+	#var dir = DirAccess.open("res://jpg_folder/").get_files()
+	# print(len(dir)
+	var dir = DirAccess.open(file_path)
+	dir.list_dir_begin()
+	var files_no:int 
 	
-	var image_text = ImageTexture.new()
-	image_text.set_image(image)
-	
-	$files/TextureRect.texture = image_text
+	while true:
+		var f = dir.get_next()
+		if f == "":
+			break
+		if not f.contains(".import"):
+			files_no += 1
+	dir.list_dir_end()
+	return files_no
 	
