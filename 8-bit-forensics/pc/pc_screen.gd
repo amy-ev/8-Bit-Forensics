33a6f2aba720774acb1d01a46c8ac2af47225835
amy-ev <amyevemy@hotmail.co.uk>
2025-05-27 16:16:34 +0100

    refactoring icon scaling

--- 8-bit-forensics/pc/pc_screen.gd
+++ 8-bit-forensics/pc/pc_screen.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var text_rect = $pc_screen
 @onready var icon = $DesktopIcon
+@export var scaled_by: Vector2
 
 func _ready() -> void:
 	icon.set_position(Vector2(15,15))
@@ -14,13 +15,13 @@ func _notification(what: int) -> void:
 
 func update_size():
 	var original_size = size
+
 	size = DisplayServer.window_get_size()
 	text_rect.size = size
 	
 	# keeping it scaled to the x axis - prevents distortion of the icon image
-	icon.get_node("icon").size = icon.get_node("icon").size / (Vector2(original_size.x -1, original_size.x -1)/Vector2(size.x-1,size.x-1))
+	scaled_by = (Vector2(original_size.x -1, original_size.x -1)/Vector2(size.x-1,size.x-1))
+	
+	icon.get_node("icon").size = icon.get_node("icon").size / scaled_by
 	icon.get_node("select/select_shape").shape.size = icon.get_node("icon").size
 	icon.get_node("select").position = icon.get_node("select/select_shape").shape.size / 2
-	
-	print(icon.size)
-	print(original_size/size)
