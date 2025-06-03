7ad00b034c91d2a4aba5f413979cf6ddfbd6b8ba
amy-ev <amyevemy@hotmail.co.uk>
2025-06-03 19:26:16 +0100

    select and search  functions implemented

--- 8-bit-forensics/python_files/json_test.py
+++ 8-bit-forensics/python_files/json_test.py
@@ -8,8 +8,6 @@ data = [
 ['fc', '00', 'f3', '5e', '46', '67', '27', 'cb', '64', '65', '19', 'ab', 'e8', '7f', 'ff', 'd9']
 ]
 
-col_num = len(data[0])
-
 result_json = {
     "rows":[
         {f'column_{i}': row[i] for i in range(len(row))}
@@ -29,4 +27,63 @@ def search(x,y):
     value = row_data[f'column_{y}']
     return value
 
-print(search(2,6))
+# select from value to value - but difficult with multiple of same value
+def select(start_val,end_val):
+
+    row_data = read["rows"]
+    reconstructed = [
+        [row[f'column_{i}'] for i in range(len(row))]
+        for row in row_data
+    ]
+
+    flat = [value for row in reconstructed for value in row]
+    start = flat.index(start_val)
+    end = flat.index(end_val)
+    
+    if start > end:
+        start, end = end, start
+
+    return flat[start:end+1]
+
+# select from start to end indexes
+def select_idx(x1,y1,x2,y2):
+
+    row_data = read["rows"]
+    reconstructed = [
+        [row[f'column_{i}'] for i in range(len(row))]
+        for row in row_data
+    ]
+
+    num_cols = len(reconstructed[0])
+    flat = [value for row in reconstructed for value in row]
+
+    start = x1 * num_cols + y1
+    end = x2 * num_cols + y2
+    
+    if start > end:
+        start, end = end, start
+
+    return flat[start:end+1]
+
+# find the x,y coordinate of the value
+def value_pos(val):
+    row_data = read["rows"]
+    reconstructed = [
+        [row[f'column_{i}'] for i in range(len(row))]
+        for row in row_data
+    ]
+    pos = None
+
+    for x, row in enumerate(reconstructed):
+        for y, value in enumerate(row):
+            if value == val and pos is None:
+                pos = (x, y)
+    return pos
+
+print(select('6c','b5'))
+print(value_pos('6c'))
+
+
+# more likely to be of use
+print(select_idx(0,5,1,0))
+print(search(0,5))
