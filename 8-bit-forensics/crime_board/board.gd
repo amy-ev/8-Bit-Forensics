20117564804d6a8bbd8b49ab4faf80d470d08f8c
amy-ev <amyevemy@hotmail.co.uk>
2025-06-19 21:30:28 +0100

    mapping out of crime_board functionalities

---
+++ 8-bit-forensics/crime_board/board.gd
@@ -0,0 +1,4 @@
+extends TextureRect
+
+
+#TODO: if specific global variables are true then add the corresponding note
---
+++ 8-bit-forensics/crime_board/note.gd
@@ -0,0 +1,7 @@
+extends TextureRect
+
+
+func _on_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton && event.is_pressed():
+		if event.is_double_click():
+			print("open note here")
