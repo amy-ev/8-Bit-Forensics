7a512c670915459803b056bc222fce913a5d993e
amy-ev <amyevemy@hotmail.co.uk>
2025-06-19 20:41:40 +0100

    button functionality placeholder

---
+++ 8-bit-forensics/start_menu/menu.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with loading save file if there is one.
+	# or starting a new file
+
+
+func _on_information_pressed() -> void:
+	pass # Replace with initialising the crime board education information.
+
+
+func _on_level_select_pressed() -> void:
+	pass # Replace with day/tool selection
+	# each level is locked until played first time 
