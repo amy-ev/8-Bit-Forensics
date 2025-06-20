389dc8041311bfe822614d65a58d1881ff987982
amy-ev <amyevemy@hotmail.co.uk>
2025-06-20 12:17:14 +0100

    global signal, unlock days and present corresponding notes on crime board

--- 8-bit-forensics/global.gd
+++ 8-bit-forensics/global.gd
@@ -1,7 +1,10 @@
 extends Node
 
 @export var magnification: int = 3
+@export var days: Array = []
 
 signal selected(selected_node:File, real_file:String)
 
 signal note_selected(note_topic:String)
+
+signal level_unlocked(day:String)
