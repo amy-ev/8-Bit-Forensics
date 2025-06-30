a87a1bc70500bf2bcb948ba150fe62f008671992
amy-ev <amyevemy@hotmail.co.uk>
2025-06-30 11:34:00 +0100

    evidence script temp resources

--- 8-bit-forensics/main/evidence.gd
+++ 8-bit-forensics/main/evidence.gd
@@ -5,6 +5,5 @@ var day = Global.unlocked +1
 func _ready() -> void:
 	$evidence.texture = load("res://assets/crime_board/notes/"+str(day)+"-note-x3.png")
 	
-
 func _on_back_pressed() -> void:
 	queue_free()
