ffe119cbd96bb1f32d2da59c4d753cd74b5da9ab
amy-ev <amyevemy@hotmail.co.uk>
2025-07-01 12:32:51 +0100

    rotation shader created

--- 8-bit-forensics/main/evidence.gd
+++ 8-bit-forensics/main/evidence.gd
@@ -4,8 +4,8 @@ var day = Global.unlocked +1
 
 func _ready() -> void:
 	# "res://assets/evidence/evidence_"+str(day)+".tscn"
-	$evidence.texture = load("res://assets/office/btn-x3.png")
-	
+	#$evidence.texture = load("res://assets/office/btn-x3.png")
+	$evidence.material
 func _on_back_pressed() -> void:
 	queue_free()
 
