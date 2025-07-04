86080774828ecbfafecca390359bb64f1aa6c71d
amy-ev <amyevemy@hotmail.co.uk>
2025-07-04 13:57:03 +0100

    book flip animation test

--- 8-bit-forensics/level_select/closed_folder.gd
+++ 8-bit-forensics/level_select/closed_folder.gd
@@ -2,7 +2,13 @@ extends TextureRect
 
 func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse && event.is_pressed():
+
+		$Sprite2D/AnimationPlayer.play("open_1")
+		
+		await $Sprite2D/AnimationPlayer.animation_finished
+		
 		queue_free()
 		get_parent().add_child(load("res://level_select/level_select_"+ str(shape_idx +1) + ".tscn").instantiate())
 		get_parent().move_child(get_node("../level_select_"+ str(shape_idx +1)),1)
+
 		#Global.emit_signal("child_joined")
--- 8-bit-forensics/level_select/open_folder_template.gd
+++ 8-bit-forensics/level_select/open_folder_template.gd
@@ -2,7 +2,18 @@ extends TextureRect
 
 func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse && event.is_pressed():
+
+		var level_num = self.name.substr(13,1)
+		
+		if int(level_num) > shape_idx +1:
+			$Sprite2D/AnimationPlayer.play("open_"+str(shape_idx +1)+"_backwards")
+		else:
+			$Sprite2D/AnimationPlayer.play("open_"+str(shape_idx +1))
+			
+		await $Sprite2D/AnimationPlayer.animation_finished
+
 		queue_free()
 		get_parent().add_child(load("res://level_select/level_select_"+ str(shape_idx +1) + ".tscn").instantiate())
 		get_parent().move_child(get_node("../level_select_"+ str(shape_idx +1)),1)
+		
 		#Global.emit_signal("child_joined")
