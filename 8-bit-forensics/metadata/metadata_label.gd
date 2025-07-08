71a13d5f7e083f8f306b5294ec8bb80127bb5de8
amy-ev <amyevemy@hotmail.co.uk>
2025-07-08 20:20:18 +0100

    test _draw function for line draw from metadata label

--- 8-bit-forensics/metadata/metadata_label.gd
+++ 8-bit-forensics/metadata/metadata_label.gd
@@ -1,5 +1,6 @@
 extends Label
 
+var draw_allowed:bool = false
 
 func _ready() -> void:
 	
@@ -17,3 +18,12 @@ func _process(delta: float) -> void:
 func _on_select_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton && event.is_pressed():
 		Global.metadata_selected.emit($".")
+		draw_allowed = true
+		queue_redraw()
+		
+func _draw() -> void:
+	if draw_allowed:
+		print("woo")
+		var pos_from = $selected.position + Vector2($selected.size.x,$selected.size.y/2)
+		var pos_to = ($selected.position + Vector2($selected.size.x,$selected.size.y/2)) + Vector2(40,0)
+		draw_dashed_line(pos_from, pos_to, Color.WHITE, 3.0,3.0)
