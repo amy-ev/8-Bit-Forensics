8571b01a891a43c34cd23ac25713f0fe875fb90f
amy-ev <amyevemy@hotmail.co.uk>
2025-07-12 13:34:28 +0100

    link file created to evidence tree

--- 8-bit-forensics/evidence/create_file.gd
+++ 8-bit-forensics/evidence/create_file.gd
@@ -1,9 +1,10 @@
 extends Panel
-
+@export var files = []
 func _ready() -> void:
 	$finished.disabled = true
 	await $animation.animation_finished
 	$finished.disabled = false
 
 func _on_finished_pressed() -> void:
+	get_parent().file_created = true
 	queue_free()
--- 8-bit-forensics/evidence/image_file.gd
+++ 8-bit-forensics/evidence/image_file.gd
@@ -1,6 +1,8 @@
 extends Panel
 
 @onready var popup = $MenuButton.get_popup()
+var msg = "file1"
+@export var file_created:bool
 
 func _ready() -> void:
 	popup.id_pressed.connect(func(id: int):
@@ -9,4 +11,10 @@ func _ready() -> void:
 func menu_popup(id):
 	if id == 0:
 		add_child(load("res://evidence/select_source.tscn").instantiate())
-	
+
+func _draw() -> void:
+	if file_created:
+		print("created")
+		#change to match the file contents of the sd card
+		var file_1 = $evidence_tree.create_item()
+		file_1.set_text(0,msg)
