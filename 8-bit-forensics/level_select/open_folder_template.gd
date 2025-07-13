f51e37ba83075f9a674dba45c6de682d8a807c3f
amy-ev <amyevemy@hotmail.co.uk>
2025-07-13 20:10:39 +0100

    level select navigation logic altered with assets

--- 8-bit-forensics/level_select/open_folder_template.gd
+++ 8-bit-forensics/level_select/open_folder_template.gd
@@ -1,5 +1,9 @@
 extends Sprite2D
 
+func _ready() -> void:
+	$back.disabled = false
+	$next.disabled = false
+	
 func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouse && event.is_pressed():
 
@@ -20,3 +24,29 @@ func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> 
 
 func _on_play_pressed() -> void:
 	print("uhoh")
+
+
+func _on_next_pressed() -> void:
+	var level_num = self.name.substr(13,1)
+	$AnimationPlayer.play("open_"+str(int(level_num) +1))
+	$next.disabled = true
+	await $AnimationPlayer.animation_finished
+
+	queue_free()
+	get_parent().add_child(load("res://level_select/level_select_"+ str(int(level_num) +1) + ".tscn").instantiate())
+	get_parent().move_child(get_node("../level_select_"+ str(int(level_num) +1)),1)
+
+
+func _on_back_pressed() -> void:
+	var level_num = self.name.substr(13,1)
+	$AnimationPlayer.play("open_"+str(int(level_num) - 1)+"_backwards")
+	$back.disabled = true
+	await $AnimationPlayer.animation_finished
+
+	queue_free()
+	if int(level_num) > 1:
+		get_parent().add_child(load("res://level_select/level_select_"+ str(int(level_num) - 1) + ".tscn").instantiate())
+		get_parent().move_child(get_node("../level_select_"+ str(int(level_num) - 1)),1)
+	else:
+		get_parent().add_child(load("res://level_select/closed_folder.tscn").instantiate())
+		get_parent().move_child(get_node("../closed_folder"),1)
