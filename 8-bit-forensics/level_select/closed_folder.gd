365d5e81602378c7e7ef720fd14cd379c2ee4f8f
amy-ev <amyevemy@hotmail.co.uk>
2025-07-13 20:16:17 +0100

    cleared up node structure

--- 8-bit-forensics/level_select/closed_folder.gd
+++ 8-bit-forensics/level_select/closed_folder.gd
@@ -1,19 +1,5 @@
 extends Sprite2D
 
-func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouse && event.is_pressed():
-
-		$AnimationPlayer.play("open_"+str(shape_idx +1))
-		
-		await $AnimationPlayer.animation_finished
-		
-		queue_free()
-		get_parent().add_child(load("res://level_select/level_select_"+ str(shape_idx +1) + ".tscn").instantiate())
-		get_parent().move_child(get_node("../level_select_"+ str(shape_idx +1)),1)
-
-		#Global.emit_signal("child_joined")
-
-
 func _on_next_pressed() -> void:
 	$AnimationPlayer.play("open_1")
 	
--- 8-bit-forensics/level_select/open_folder_template.gd
+++ 8-bit-forensics/level_select/open_folder_template.gd
@@ -3,28 +3,6 @@ extends Sprite2D
 func _ready() -> void:
 	$back.disabled = false
 	$next.disabled = false
-	
-func _on_tabs_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouse && event.is_pressed():
-
-		var level_num = self.name.substr(13,1)
-		
-		if int(level_num) > shape_idx +1:
-			$AnimationPlayer.play("open_"+str(shape_idx +1)+"_backwards")
-		else:
-			$AnimationPlayer.play("open_"+str(shape_idx +1))
-			
-		await $AnimationPlayer.animation_finished
-
-		queue_free()
-		get_parent().add_child(load("res://level_select/level_select_"+ str(shape_idx +1) + ".tscn").instantiate())
-		get_parent().move_child(get_node("../level_select_"+ str(shape_idx +1)),1)
-		
-		#Global.emit_signal("child_joined")
-
-func _on_play_pressed() -> void:
-	print("uhoh")
-
 
 func _on_next_pressed() -> void:
 	var level_num = self.name.substr(13,1)
