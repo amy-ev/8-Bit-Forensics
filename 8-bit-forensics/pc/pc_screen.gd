81d0cc3dab0c2d88922c8fc9a13a0f6a5254d49b
amy-ev <amyevemy@hotmail.co.uk>
2025-07-14 22:44:58 +0100

    keyboard functionality

--- 8-bit-forensics/pc/pc_screen.gd
+++ 8-bit-forensics/pc/pc_screen.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 func _ready() -> void:
+	scale = scale * Utility.window_mode()
 	var day = Global.unlocked + 1
 	await $screen_animation.animation_finished
-	match day:
-		1:
-			add_child(load("res://evidence/image_file.tscn").instantiate())
-		2:
-			add_child(load("res://evidence/image_file.tscn").instantiate())
-			add_child(load("res://pc/desktop_icon.tscn").instantiate())
-	scale = scale * Utility.window_mode()
+	#match day:
+		#1:
+			#add_child(load("res://evidence/image_file.tscn").instantiate())
+		#2:
+			#add_child(load("res://evidence/image_file.tscn").instantiate())
+			#add_child(load("res://pc/desktop_icon.tscn").instantiate())
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_action_pressed("fullscreen"):
