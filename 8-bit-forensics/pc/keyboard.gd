930c8bf2b77cb87d590d7c32b89153591d259fb9
amy-ev <amyevemy@hotmail.co.uk>
2025-07-15 09:37:53 +0100

    finished remaining keyboard keys

--- 8-bit-forensics/pc/keyboard.gd
+++ 8-bit-forensics/pc/keyboard.gd
@@ -3,12 +3,13 @@ extends TextureRect
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey && event.is_pressed():
 		print(event.as_text_keycode())
-		if event.as_text_keycode().contains("Shift+"):
-			var key = event.as_text_keycode().substr(6,-1)
-			print(key)
-			$shift_animation.play("Shift") 
-			$key_press.play(key)
-		else:
-			if $key_press.is_playing():
-				await $key_press.animation_finished 
-			$key_press.play(event.as_text_keycode())
+		if $key_press.has_animation(event.as_text_keycode()):
+			if event.as_text_keycode().contains("Shift+"):
+				var key = event.as_text_keycode().substr(6,-1)
+				print(key)
+				$shift_animation.play("Shift") 
+				$key_press.play(key)
+			else:
+				if $key_press.is_playing():
+					await $key_press.animation_finished 
+				$key_press.play(event.as_text_keycode())
