26ceebae5956e7c4364f4a332a6398e7718212c1
amy-ev <amyevemy@hotmail.co.uk>
2025-07-20 11:11:59 +0100

    reformatting of crime board assets and dialogue arrows

--- 8-bit-forensics/dialogue/helper.gd
+++ 8-bit-forensics/dialogue/helper.gd
@@ -13,6 +13,9 @@ func _on_item_select(selected:Node):
 	if help_pressed:
 		var dialogue = _dialogue.instantiate()
 		add_child(dialogue)
+		dialogue.get_node("npc_normal").set_visible(true)
+		dialogue.get_node("npc_bad").set_visible(false)
+		dialogue.get_node("npc_good").set_visible(false)
 		dialogue.start(selected.name)
 		#TODO: match case for the selected items and what should be said
 		#match selected.name:
@@ -24,11 +27,13 @@ func _on_option_confirmed(is_correct:bool):
 	var dialogue = _dialogue.instantiate()
 	add_child(dialogue)
 	if is_correct:
-		dialogue.get_node("npc_normal").set_visible(true)
+		dialogue.get_node("npc_normal").set_visible(false)
 		dialogue.get_node("npc_bad").set_visible(false)
+		dialogue.get_node("npc_good").set_visible(true)
 		dialogue.start("correct!")
 	else:
 		dialogue.get_node("npc_normal").set_visible(false)
+		dialogue.get_node("npc_good").set_visible(false)
 		dialogue.get_node("npc_bad").set_visible(true)
 		dialogue.start("wrong")
 func _on_button_pressed() -> void:
