3607325b407ab5f6e1a8c48d5c627718851e0b7c
amy-ev <amyevemy@hotmail.co.uk>
2025-08-11 22:07:00 +0100

    renamed unused script folder

--- 8-bit-forensics/python_files_old/start.bat
+++
@@ -1,4 +0,0 @@
-@REM start /d "C:\Users\Amy\Desktop\8-Bit-Forensics\HxD.exe"
-cd C:\Users\Amy\Desktop\8-Bit-Forensics\8-bit-forensics\python_files
-python server.py
-exit
---
+++ 8-bit-forensics/test_files/start.bat
@@ -0,0 +1,3 @@
+cd %appdata%/Godot/app_userdata/8-Bit-Forensics/python_files
+python server.py
+exit
