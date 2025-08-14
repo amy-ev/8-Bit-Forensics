447f08427ea93b4b3aa6918554201ed33a954296
amy-ev <amyevemy@hotmail.co.uk>
2025-08-14 11:29:55 +0100

    background music and sfx audio mute buttons implemented

--- 8-bit-forensics/main/escape_screen.gd
+++ 8-bit-forensics/main/escape_screen.gd
@@ -7,3 +7,27 @@ func _on_exit_pressed() -> void:
 
 func _on_cancel_pressed() -> void:
 	queue_free()
+
+
+func _on_audio_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		audio.dialogue.set_volume_db(-80.0) 
+		audio.dialogue_2.set_volume_db(-80.0) 
+		audio.select.set_volume_db(-80.0) 
+		
+	else:
+		audio.dialogue.set_volume_db(0.0) 
+		audio.dialogue_2.set_volume_db(0.0) 
+		audio.select.set_volume_db(0.0) 
+
+func _on_music_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		audio.background.set_volume_db(-80.0) 
+	else:
+		audio.background.set_volume_db(-11.0) 
+
+func _on_music_mouse_entered() -> void:
+	audio.select.play(0.04)
+
+func _on_audio_mouse_entered() -> void:
+	audio.select.play(0.04)
--- 8-bit-forensics/start_menu/menu.gd
+++ 8-bit-forensics/start_menu/menu.gd
@@ -21,3 +21,33 @@ func _on_level_select_pressed() -> void:
 func _on_credits_pressed() -> void:
 
 	get_tree().change_scene_to_file("res://main/credits.tscn")
+
+
+func _on_audio_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_music_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		audio.background.set_volume_db(-80.0) 
+	else:
+		audio.background.set_volume_db(-11.0) 
+
+
+func _on_audio_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		audio.dialogue.set_volume_db(-80.0) 
+		audio.dialogue_2.set_volume_db(-80.0) 
+		audio.select.set_volume_db(-80.0) 
+		
+	else:
+		audio.dialogue.set_volume_db(0.0) 
+		audio.dialogue_2.set_volume_db(0.0) 
+		audio.select.set_volume_db(0.0) 
+
+
+func _on_audio_mouse_entered() -> void:
+	audio.select.play(0.04)
+
+func _on_music_mouse_entered() -> void:
+	audio.select.play(0.04)
